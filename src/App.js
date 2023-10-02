959a66a6f0ab60d40a5cb9e89d8694b6ebdb36ba
FeZanette <fernandazanette1975@gmail.com>
2023-10-02 16:11:44 -0300

    Aula integração APIs I template finalizada

--- src/App.js
+++ src/App.js
@@ -1,38 +1,76 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AddUsuario from "./Componentes/AddUsuario/AddUsuario";
 import Usuario from "./Componentes/Usuario/Usuario";
+import axios from "axios";
 
-const usuariosLocal = [
-  {
-    id: 1,
-    name: "Muri"
-  },
-  {
-    id: 2,
-    name: "Paulinha"
-  },
-  {
-    id: 3,
-    name: "Marcelo"
-  },
-  {
-    id: 4,
-    name: "Rodrigo"
-  },
-]
+// APAGAR USUÁRIOS MOCKADOS
+// const usuariosLocal = [
+//   {
+//     id: 1,
+//     name: "Muri"
+//   },
+//   {
+//     id: 2,
+//     name: "Paulinha"
+//   },
+//   {
+//     id: 3,
+//     name: "Marcelo"
+//   },
+//   {
+//     id: 4,
+//     name: "Rodrigo"
+//   },
+// ]
 
 function App() {
-  const [usuarios, setUsuarios] = useState(usuariosLocal)
+  const [usuarios, setUsuarios] = useState([]);
+
+  // GUARDAR REQUISIÇÃO EM UMA FUNÇÃO:
+  const getAllUsers = () => {
+    axios
+      .get(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
+        {
+          headers: {
+            Authorization: "fernanda-zanette-krexu",
+          },
+        }
+      )
+      .then((response) => {
+        // console.log(response.data);
+        setUsuarios(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  };
+  // console.log(getAllUsers);
+
+  // CHAMAR A FUNÇÃO no useEffect:
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
   return (
     <>
-      <p>Para esta aula usaremos a <a href="https://documenter.getpostman.com/view/7549981/SzfCT5G2#intro" target="_blank" rel="noreferrer">API Labenusers</a></p>
-      <AddUsuario />
-      <hr/>
+      <p>
+        Para esta aula usaremos a{" "}
+        <a
+          href="https://documenter.getpostman.com/view/7549981/SzfCT5G2#intro"
+          target="_blank"
+          rel="noreferrer"
+        >
+          API Labenusers
+        </a>
+      </p>
+      <AddUsuario getAllUsers={getAllUsers}/>
+      <hr />
       {usuarios.map((usuario) => {
-        return <Usuario key={usuario.id} usuario={usuario} />
+        return <Usuario key={usuario.id} usuario={usuario}/>;
       })}
     </>
-  )
+  );
 }
 
 export default App;
--- src/Componentes/Usuario/Usuario.js
+++ src/Componentes/Usuario/Usuario.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const User = styled.div`
@@ -6,12 +7,79 @@ const User = styled.div`
   margin-top: 8px;
   width: 350px;
   padding: 8px;
-`
+`;
 function Usuario(props) {
   const [usuario, setUsuario] = useState(props.usuario);
   const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
   const [editar, setEditar] = useState(false);
+
+  // console.log(usuario)
+
+  const body = {
+    name: nome,
+    email: email,
+  };
+
+  const headers = {
+    headers: {
+      Authorization: "fernanda-zanette-krexu",
+    },
+  };
+
+  // GUARDAR REQUISIÇÃO EM UMA FUNÇÃO:
+  const getUserById = () => {
+    axios
+      .get(
+        `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${usuario.id}`,
+        headers
+      )
+      .then((response) => {
+        // console.log(response.data);
+        setUsuario(response.data);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
+
+  useEffect(() => {
+    getUserById();
+  }, []);
+
+  const editUser = () => {
+    axios
+      .put(
+        `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${usuario.id}`,
+        body,
+        headers
+      )
+      .then(() => {
+        alert("Dados alterados com sucesso!");
+        setNome("");
+        setEmail("");
+        setEditar(!editar);
+        getUserById();
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  };
+
+  const deleteUser = () => {
+    axios
+      .delete(
+        `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${usuario.id}`,
+        headers
+      )
+      .then(() => {
+        props.getAllUsers();
+        alert("Usuário deletado com sucesso!");
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  };
 
   return (
     <User>
@@ -19,18 +87,30 @@ function Usuario(props) {
         <div>
           <p>Nome: {usuario.name}</p>
           <p>E-mail: {usuario.email}</p>
-          <input value={nome} onChange={(e) => setNome(e.target.value)} />
-          <input value={email} onChange={(e) => setEmail(e.target.value)} />
-          <button>Enviar alterações</button>
+          <input
+            placeholder="Nome"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+          <input
+            placeholder="E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button onClick={editUser}>Enviar alterações</button>
         </div>
       ) : (
         <>
-          <p><strong>Nome:</strong> {usuario.name}</p>
-          <p><strong>E-mail:</strong> {usuario.email}</p>
+          <p>
+            <strong>Nome:</strong> {usuario.name}
+          </p>
+          <p>
+            <strong>E-mail:</strong> {usuario.email}
+          </p>
         </>
       )}
       <button onClick={() => setEditar(!editar)}>Editar</button>
-      <button>Excluir</button>
+      <button onClick={deleteUser}>Excluir</button>
     </User>
   );
 }
